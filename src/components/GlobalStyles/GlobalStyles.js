2dafb6af616ea62723f94e9ae9ff3c09322ef0dc
Thanh Hoan <thanhhoan12390@gmail.com>
2024-08-02 22:09:32 +0700

    Build default layout

--- src/components/GlobalStyles/GlobalStyles.js
+++ src/components/GlobalStyles/GlobalStyles.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 
 import './GlobalStyles.scss';
+import '~/assets/css/grid.css';
 
 function GlobalStyles({ children }) {
     return children;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,28 @@ import styles from './Home.module.scss';
 const cx = classNames.bind(styles);
 
 function Home() {
-    return <h2 className={cx('wrapper')}>Home Page</h2>;
+    return (
+        <div className={cx('grid', 'wrapper')}>
+            {/* Banner */}
+            <div className={cx('banner')}></div>
+
+            {/* Home Content */}
+            <div className={cx('row', 'content')}>
+                <div className={cx('col', 'l-3', 'm-6', 'c-6')}>
+                    <div className={cx('item')}></div>
+                </div>
+                <div className={cx('col', 'l-3', 'm-6', 'c-6')}>
+                    <div className={cx('item')}></div>
+                </div>
+                <div className={cx('col', 'l-3', 'm-6', 'c-6')}>
+                    <div className={cx('item')}></div>
+                </div>
+                <div className={cx('col', 'l-3', 'm-6', 'c-6')}>
+                    <div className={cx('item')}></div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
